68b37bad68e20991e8b72f216f923f2cc5b42ac3
DepressedProtagonist <75769990+DepressedProtagonist@users.noreply.github.com>
2022-05-12 22:32:52 +1000

    new

--- Scripts/app.js
+++
@@ -1,8 +0,0 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content');
-
-function PageTransitions() {
-  //Button click active class
-}
